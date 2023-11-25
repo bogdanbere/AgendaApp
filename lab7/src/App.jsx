fac9028b9859a8502f3d32405ca5c79e5ee2ee5c
bogdanbere <bogdanbere@gmail.com>
2023-11-25 10:46:48 +0200

    created formular component and added react router dom package

--- lab7/src/App.jsx
+++ lab7/src/App.jsx
@@ -1,42 +1,27 @@
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/esm/Container';
 import Program from './program';
-import './App.css'
-
-const lista = [
-  {
-    id: 1,
-    ora: "10:30",
-    loc: "Sala G08",
-    titlu: "Întâlnire cu presa",
-    descriere: "Nu e cazul."
-  },
-  {
-    id: 2,
-    ora: "11:00",
-    loc: "Birou",
-    titlu: "Pauză de cafea",
-    descriere: "Nu e cazul."
-  },
-  {
-    id: 3,
-    ora: "12:00",
-    loc: "Birou",
-    titlu: "Pregătire curs",
-    descriere: "Pregătirea unui studiu de caz."
-  }
-];
+import Formular from "./formular";
 
 export default function App() {
+  const [lista, setLista] = useState([]);
+
   const stergActiv = (id) => console.log("Sterg: " + id);
+
+  const adaugaActiv = (act) => {
+    act.id = lista.length + 1;
+    setLista([...lista, act]);
+  };
+
   const stil = {
     container: { maxWidth: "700px" },
-    h2: { textAlign: "center" }
   };
 
   return (
     <Container style={stil.container}>
-      <Program activitati={lista} sterge={stergActiv} />
+        <Program activitati={lista} sterge={stergActiv} />
+        <Formular transmit={adaugaActiv} />
     </Container>
   );
 }
