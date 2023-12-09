fddc59242d8d85fe128dd75b438a153e9a8e3ac7
bogdanbere <bogdanbere@gmail.com>
2023-12-09 13:43:23 +0200

    added lsita.json and edited server to use json file instead

--- agenda/backend/server.js
+++ agenda/backend/server.js
@@ -1,39 +1,27 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
+
 const port = 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const lista = [
-    {
-        id: 1,
-        ora: "10:30",
-        loc: "Sala G08",
-        titlu: "Întâlnire cu presa",
-        descriere: "Nu e cazul."
-    },
-    {
-        id: 2,
-        ora: "11:00",
-        loc: "Birou",
-        titlu: "Pauză de cafea",
-        descriere: "Nu e cazul."
-    },
-    {
-        id: 3,
-        ora: "12:00",
-        loc: "Birou",
-        titlu: "Pregătire curs",
-        descriere: "Pregătirea unui studiu de caz."
-    }
-];
+let lista = [];
+
+const citesteLista = () => {
+    const response = fs. readFileSync(`./lista.json`, "utf-8");
+    lista = JSON.parse(response);
+};
+
+citesteLista();
 
 const listaJson = JSON.stringify(lista);
 
 app.get("/", (req, res) => {
-    res.status(200).send(listaJson);
+    citesteLista();
+    res.status(200).send(lista);
 });
 
 app.listen(port, () => {
